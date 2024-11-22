71a21be440198169469254010565b8fb3660815e
kutikq <kutenkov5yandex.ru@mail.ru>
2024-11-22 21:09:51 +0300

    Event classes

--- Classes.py
+++ Classes.py
@@ -1,2 +1,31 @@
+from datetime import datetime
+
+
 class Event:
-    def__init__()
+    def __init__(self, name: str, date_time: datetime, venue: 'Venue'):
+        self.name=name
+        self.datetime=date_time
+        self.Venue=venue
+
+class Venue:
+    def __init__(self, name:str, place:str, capacity: int):
+        self.name = name
+        self.place = place
+        self.capacity = capacity
+        self.seats: List['Seat'] = [] 
+
+class Seat:
+    def __init__(self, row:int , number:int):
+        self.row = row
+        self.number = number
+        self.is_avaible = True
+
+    def book (self):
+        while self.is_avaible:
+            try:
+                print(f'Seat is booked')
+            except Exception as e:
+                print(f'Error! {e}')
+
+
+
